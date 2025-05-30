29e39697cd50e1fbd836ef3c3dde94abe0d65a80
VishwasRYPM <vikumar@royalyorkpm.com>
2025-05-30 19:24:22 +0530

    build issue fixes

--- src/ui/icons/WhirlpoolIcon.tsx
+++ src/ui/icons/WhirlpoolIcon.tsx
@@ -10,8 +10,8 @@ interface WhirlpoolIconProps {
 const WhirlpoolIcon = ({ 
   width = 24, 
   height = 24, 
-  className = '',
-  color = '#001D3D' 
+  //className = '',
+  //color = '#001D3D' 
 }: WhirlpoolIconProps) => {
   return (
    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none">
